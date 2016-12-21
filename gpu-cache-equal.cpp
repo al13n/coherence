1afddabcfd7c6f7ce41cddef86a2d268ecf45fdd
Akshit Poddar <apoddar@cs.stonybrook.edu>
2016-12-21 13:47:05 -0500

    rename _cache to _dir

--- gpu-cache-equal.cpp
+++ gpu-cache-equal.cpp
@@ -5,7 +5,7 @@
 int main(){
 	string instruction;
 	gpu_simulator _gpu(1<<GPU_ADDRESS_LEN);
-	dir_simulator _cache;
+	dir_simulator _dir;
 
 	while(getline(cin, instruction)) {
 		stringstream ss(instruction);
@@ -19,16 +19,16 @@ int main(){
 		{
 			// The cpu needs to access some memory, ask the coherence directory about it
 			//Consult cache
-			if (_cache.exists(address))
+			if (_dir.exists(address))
 			{
 				if (!_gpu.address_exists(address))
-					_cache.false_positive(address);
+					_dir.false_positive(address);
 			}
 			
 		} else {
 			if (type == "MEMRD64B")
 			{	
-				_cache.insert(address);
+				_dir.insert(address);
 				UL gpu_address = __getaddress_gpu__(address);
 				UL tag = __gettag_gpu__(address);
 				
@@ -41,9 +41,9 @@ int main(){
 		}
 	}
 	
-	cout << "Directory usage/consult: " << _cache.get_consult() << endl;
-	cout << "Directory size: " << _cache.size() << endl;
-	cout << "False positives: " << _cache.get_fp() << endl;
-	if (_cache.get_fp())	_cache.display_fps();	
+	cout << "Directory usage/consult: " << _dir.get_consult() << endl;
+	cout << "Directory size: " << _dir.size() << endl;
+	cout << "False positives: " << _dir.get_fp() << endl;
+	if (_dir.get_fp())	_dir.display_fps();	
 	return 0;
 }
